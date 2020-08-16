3cb07388a345b7964a86c25213253eb132ff7eb6
GeorgeDimos <dimos@uth.gr>
2020-08-16 12:26:45 +0300

    Fixed negative numbers. Should add tests.

--- Calculator.java
+++ Calculator.java
@@ -9,87 +9,76 @@ import java.util.*;
  * @author George
  */
 public class Calculator {
-    /**
-     * @param args the command line arguments
-     */
-    private NodeTree tree;
-    
-    public Calculator (String input) throws IllegalArgumentException {
-	if(!checkValidInput(input)){
-	    throw new IllegalArgumentException();
-	}
-	tree = new NodeTree(input);
-    }
-    
-    @Override
-    public String toString(){
-	return tree.toString();
-    }
-    
-    public String toDotString(){
-	return tree.toDotString();
-    }
-    
-    public double calculate(){
-	return tree.calculate();
-    }
-    
-    private boolean checkValidInput(String input){
-	final String noWhitespaceInput = input.replaceAll("\\s","");
-	String op = "[\\+\\-\\*\\/\\^]";
-	String num = "\\d+(.\\d+)?";
-	String openParen = "\\(";
-	String closeParen = "\\)";
-	String sign="[\\+\\-]?";
-	String OR = "|";
-	
-	String myRegex = "(("+sign+openParen+")*"+sign+num+"("+closeParen+OR+"("+op+num+"))*("+op+openParen+")?)+";
+	/**
+	 * @param args the command line arguments
+	 */
+	private NodeTree tree;
 
-	if(!noWhitespaceInput.matches(myRegex)){
-	    System.out.println("REJECTED");
-	    return false;
+	public Calculator(String input) throws IllegalArgumentException {
+		if (!checkValidInput(input)) {
+			throw new IllegalArgumentException();
+		}
+		tree = new NodeTree(input);
 	}
 
-	int open = 0;
-	for(int i=0;i<noWhitespaceInput.length();i++){
-	    if(noWhitespaceInput.charAt(i)=='('){
-		open++;
-	    }
-	    if(noWhitespaceInput.charAt(i)==')'){
-		if(open==0){
-		    return false;
-		}
-		open--;
-	    }
+	@Override
+	public String toString() {
+		return tree.toString();
 	}
-	return open==0;
-    }
-    
-    /*public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	System.out.print("Enter math expression or \"0\" to stop: ");
-	String expr = sc.nextLine();
-	Calculator c;
-	while(!expr.equals("0")){
-	    try{
-		c = new Calculator(expr);
-		System.out.println("or else is: " + c.toString());
-		System.out.println("result: " + c.calculate());
-		try (PrintWriter pfile = new PrintWriter("ArithmeticExpression.dot")) {
-		    pfile.println(c.toDotString());
-		    System.out.println("PRINT DOT FILE OK!");
-		}
-		catch(FileNotFoundException ex) {
-		    System.err.println("Unable to write dotString!!!");
-		    System.exit(1);
-		}
-	    }
-	    catch(IllegalArgumentException ex){
-		System.out.println("Invalid input. Try again.");
-	    }
-	    System.out.print("Enter math expression or \"0\" to stop: ");
-	    expr = sc.nextLine();
+
+	public String toDotString() {
+		return tree.toDotString();
 	}
-    }*/
-    
+
+	public double calculate() {
+		return tree.calculate();
+	}
+
+	private boolean checkValidInput(String input) {
+		final String noWhitespaceInput = input.replaceAll("\\s", "");
+		String op = "[\\+\\-\\*\\/\\^]";
+		String num = "\\d+(.\\d+)?";
+		String openParen = "\\(";
+		String closeParen = "\\)";
+		String sign = "[\\+\\-]?";
+		String OR = "|";
+
+		String myRegex = "((" + sign + openParen + ")*" + sign + num + "(" + closeParen + OR + "(" + op + num + "))*("
+				+ op + openParen + ")?)+";
+
+		if (!noWhitespaceInput.matches(myRegex)) {
+			System.out.println("REJECTED");
+			return false;
+		}
+
+		int open = 0;
+		for (int i = 0; i < noWhitespaceInput.length(); i++) {
+			if (noWhitespaceInput.charAt(i) == '(') {
+				open++;
+			}
+			if (noWhitespaceInput.charAt(i) == ')') {
+				if (open == 0) {
+					return false;
+				}
+				open--;
+			}
+		}
+		return open == 0;
+	}
+
+	/*
+	 * public static void main(String[] args) { Scanner sc = new Scanner(System.in);
+	 * System.out.print("Enter math expression or \"0\" to stop: "); String expr =
+	 * sc.nextLine(); Calculator c; while(!expr.equals("0")){ try{ c = new
+	 * Calculator(expr); System.out.println("or else is: " + c.toString());
+	 * System.out.println("result: " + c.calculate()); try (PrintWriter pfile = new
+	 * PrintWriter("ArithmeticExpression.dot")) { pfile.println(c.toDotString());
+	 * System.out.println("PRINT DOT FILE OK!"); } catch(FileNotFoundException ex) {
+	 * System.err.println("Unable to write dotString!!!"); System.exit(1); } }
+	 * catch(IllegalArgumentException ex){
+	 * System.out.println("Invalid input. Try again."); }
+	 * System.out.print("Enter math expression or \"0\" to stop: "); expr =
+	 * sc.nextLine(); } }
+	 */
+
 }
--- NodeTree.java
+++ NodeTree.java
@@ -5,220 +5,218 @@ package calculator;
  * @author George
  */
 public class NodeTree {
-    private final nodeT root;
-        
-    public NodeTree(String input){
-	root = createTree(trimParen(input.replaceAll("\\s", "")));
-    }
-    
-    private nodeT createTree(String input){
-	
-	input = trimParen(input);
-	String currentVal;
-	nodeT left, right;
-	
-	int operatorIndex = getNextOperatorIndex(input);
-	if(operatorIndex !=-1){
-	    currentVal = String.valueOf(input.charAt(operatorIndex));
-	    left = createTree(operatorIndex==0 ? "0" : input.substring(0, operatorIndex));
-	    right = createTree(input.substring(operatorIndex+1, input.length()));
+	private final nodeT root;
+
+	public NodeTree(String input) {
+		root = createTree(trimParen(input.replaceAll("\\s", "")));
 	}
-	else{
-	    currentVal = input;
-	    left = null;
-	    right = null;
-	}
-	
-	return new nodeT(currentVal, left, right);
-    }
-   
-    private String trimParen(String input){
-	if(input.length()<2){
-	    return input;
-	}
-	
-	if(input.charAt(0)!='(' || input.charAt(input.length()-1)!=')'){
-	    return input;
-	}
-	
-	if(!canRemoveParenthesis(input)){
-	    return input;
-	}
-	
-	return trimParen(input.substring(1, input.length()-1));
-	
-    }
-    
-    private boolean canRemoveParenthesis(String input){
-	
-	int open=0;
-	for(int i=1; i<input.length()-1;i++){
-	    if( input.charAt(i)=='('){
-		open++;
-	    }
-	    else if(input.charAt(i)==')' && open>0){
-		open--;
-	    }
-	}
-	return open==0;
-    }
-    
-    private int getNextOperatorIndex(String input){
-	int open = 0;
-	int[] index = new int[5];
-	for(int i=0; i<5; i++){
-	    index[i]=-1;
-	}
-	for(int i=0; i<input.length(); i++){
-	    if( input.charAt(i)=='('){
-		open++;
-	    }
-	    if( input.charAt(i)==')'){
-		open--;
-	    }
-	    if(open==0){
-		switch (input.charAt(i)) {
-		    case '-':
-			index[0] = i; 
-			break;
-		    case '+':
-			index[1] = i;
-			break;
-		    case '/':
-			index[2] = i;
-			break;
-		    case '*':
-			index[3] = i;
-			break;
-		    case '^':
-			index[4] = i;
-			break;
-		    default:
-			break;
+
+	private nodeT createTree(String input) {
+
+		input = trimParen(input);
+		String currentVal;
+		nodeT left, right;
+
+		int operatorIndex = getNextOperatorIndex(input);
+		if (operatorIndex != -1) {
+			currentVal = String.valueOf(input.charAt(operatorIndex));
+			left = createTree(operatorIndex == 0 ? "0" : input.substring(0, operatorIndex));
+			right = createTree(input.substring(operatorIndex + 1, input.length()));
+		} else {
+			currentVal = input;
+			left = null;
+			right = null;
 		}
-	    }
+
+		return new nodeT(currentVal, left, right);
 	}
-	for(int i=0;i<5;i++){
-	    if(index[i]!=-1){
-		return index[i];
-	    }
+
+	private String trimParen(String input) {
+		if (input.length() < 2) {
+			return input;
+		}
+
+		if (input.charAt(0) != '(' || input.charAt(input.length() - 1) != ')') {
+			return input;
+		}
+
+		if (!canRemoveParenthesis(input)) {
+			return input;
+		}
+
+		return trimParen(input.substring(1, input.length() - 1));
+
 	}
-	return -1;
-    }
-    
-    public double calculate(){
-	return calculate(root);
-    }
-    
-    private double calculate(nodeT node){
-	if(node.getLeft()==null || node.getRight()==null){
-	    return Double.parseDouble(node.getVal());
+
+	private boolean canRemoveParenthesis(String input) {
+
+		int open = 0;
+		for (int i = 1; i < input.length() - 1; i++) {
+			if (input.charAt(i) == '(') {
+				open++;
+			} else if (input.charAt(i) == ')' && open > 0) {
+				open--;
+			}
+		}
+		return open == 0;
 	}
-	
-	double left = calculate(node.getLeft());
-	double right = calculate(node.getRight());
-		
-	switch (node.getVal()){
-	    case "+":
-		return left+right;
-	    case "-":
-		return left-right;
-	    case "*":
-		return left*right;
-	    case "/":
-		return left/right;
-	    case "^":
-		return Math.pow(left,right);
-	    default:
-		return 0;
+
+	private int getNextOperatorIndex(String input) {
+		int open = 0;
+		int[] index = new int[5];
+		for (int i = 0; i < 5; i++) {
+			index[i] = -1;
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == '(') {
+				open++;
+			}
+			if (input.charAt(i) == ')') {
+				open--;
+			}
+			if (open == 0) {
+				switch (input.charAt(i)) {
+					case '+':
+						index[0] = i;
+						break;
+					case '-':
+						index[1] = i;
+						break;
+					case '/':
+						index[2] = i;
+						break;
+					case '*':
+						index[3] = i;
+						break;
+					case '^':
+						index[4] = i;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		for (int i = 0; i < 5; i++) {
+			if (index[i] != -1) {
+				return index[i];
+			}
+		}
+		return -1;
 	}
-    }
-    
-    @Override
-    public String toString(){
-	return toString(root);
-    }
-    
-    private String toString(nodeT cur){
-	StringBuilder str = new StringBuilder();
-	if(cur!=null){
-	    
-	    if(cur.getLeft()!=null){
-		str.append("(");
-		str.append(toString(cur.getLeft()));
-	    }
-	    
-	    str.append(cur.getVal());
-	    
-	    if(cur.getRight()!=null){
-		str.append(toString(cur.getRight()));
-		str.append(")");
-	    }
-	    
+
+	public double calculate() {
+		return calculate(root);
 	}
-	return str.toString();
-    }
-    
-    public String toDotString(){
-	StringBuilder str = new StringBuilder();
-	str.append("digraph ArithmeticExpressionTree {\n" + "label=\"Arithmetic Expression\"\n");
-	str.append(toDotString(root));
-	str.append("}");
-	return str.toString();
-    }
-    
-    private String toDotString(nodeT node){
-	StringBuilder str = new StringBuilder();
-	if(node!=null){
-	    str.append(node.hashCode());
-	    str.append(" [label=\"");
-	    str.append(node.getVal());
-	    str.append("\", shape=circle, color=black]\n");
-	    
-	    if(node.getLeft()!=null){
-		str.append(node.hashCode()).append("->").append(node.getLeft().hashCode()).append("\n");
-		str.append(toDotString(node.getLeft()));
-	    }
-	    if(node.getRight()!=null){
-		str.append(node.hashCode()).append(" -> ").append(node.getRight().hashCode()).append("\n");
-		str.append(toDotString(node.getRight()));
-	    }
+
+	private double calculate(nodeT node) {
+		if (node.getLeft() == null || node.getRight() == null) {
+			return Double.parseDouble(node.getVal());
+		}
+
+		double left = calculate(node.getLeft());
+		double right = calculate(node.getRight());
+
+		switch (node.getVal()) {
+			case "+":
+				return left + right;
+			case "-":
+				return left - right;
+			case "*":
+				return left * right;
+			case "/":
+				return left / right;
+			case "^":
+				return Math.pow(left, right);
+			default:
+				return 0;
+		}
 	}
-	return str.toString();
-    }
-    
-    private class nodeT {
-	private nodeT left, right;
-	private String val;
-	
-	public nodeT(String val, nodeT left, nodeT right){
-	    this.left = left;
-	    this.right = right;
-	    this.val = val;
+
+	@Override
+	public String toString() {
+		return toString(root);
 	}
-	
-	protected String getVal(){
-	    return val;
+
+	private String toString(nodeT cur) {
+		StringBuilder str = new StringBuilder();
+		if (cur != null) {
+
+			if (cur.getLeft() != null) {
+				str.append("(");
+				str.append(toString(cur.getLeft()));
+			}
+
+			str.append(cur.getVal());
+
+			if (cur.getRight() != null) {
+				str.append(toString(cur.getRight()));
+				str.append(")");
+			}
+
+		}
+		return str.toString();
 	}
-	
-	protected nodeT getLeft(){
-	    return left;
+
+	public String toDotString() {
+		StringBuilder str = new StringBuilder();
+		str.append("digraph ArithmeticExpressionTree {\n" + "label=\"Arithmetic Expression\"\n");
+		str.append(toDotString(root));
+		str.append("}");
+		return str.toString();
 	}
-	
-	protected nodeT getRight(){
-	    return right;
+
+	private String toDotString(nodeT node) {
+		StringBuilder str = new StringBuilder();
+		if (node != null) {
+			str.append(node.hashCode());
+			str.append(" [label=\"");
+			str.append(node.getVal());
+			str.append("\", shape=circle, color=black]\n");
+
+			if (node.getLeft() != null) {
+				str.append(node.hashCode()).append("->").append(node.getLeft().hashCode()).append("\n");
+				str.append(toDotString(node.getLeft()));
+			}
+			if (node.getRight() != null) {
+				str.append(node.hashCode()).append(" -> ").append(node.getRight().hashCode()).append("\n");
+				str.append(toDotString(node.getRight()));
+			}
+		}
+		return str.toString();
 	}
-	
-	protected void setVal(String val){
-	    this.val = val;
+
+	private class nodeT {
+		private nodeT left, right;
+		private String val;
+
+		public nodeT(String val, nodeT left, nodeT right) {
+			this.left = left;
+			this.right = right;
+			this.val = val;
+		}
+
+		protected String getVal() {
+			return val;
+		}
+
+		protected nodeT getLeft() {
+			return left;
+		}
+
+		protected nodeT getRight() {
+			return right;
+		}
+
+		protected void setVal(String val) {
+			this.val = val;
+		}
+
+		protected void setLeft(nodeT left) {
+			this.left = left;
+		}
+
+		protected void setRight(nodeT right) {
+			this.right = right;
+		}
 	}
-	
-	protected void setLeft(nodeT left){
-	    this.left = left;
-	}
-	
-	protected void setRight(nodeT right){
-	    this.right = right;
-	}
-    }
 }
